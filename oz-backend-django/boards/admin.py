94e1adac6e1f312c90ed104b4801576eb0314e12
lss018000 <lss018000@gmail.com>
2024-02-19 15:06:12 +0900

    20240219-2

--- oz-backend-django/boards/admin.py
+++ oz-backend-django/boards/admin.py
@@ -3,4 +3,19 @@ from .models import Board
 
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('title', 'writer', 'date', 'likes', 'content','updated_at', 'created_at')
+    list_filter = ('date', 'writer')
+    search_fields = ('title', 'content')
+    ordering = ('-date',)
+    readonly_fields = ('writer',)
+    fieldsets = (
+		(None, {'fields': ('title', 'content')}),
+		('추가 옵션', {'fields': ('writer', 'likes', 'reviews'), 'classes': ('collapse',)}),
+	)
+    list_per_page = 1
+    actions = ('increment_likes',)
+    def increment_likes(self, request, queryset):
+        for board in queryset:
+            board.likes += 1
+            board.save()
+    increment_likes.short_description = "선택된 게시글의 좋아요 수 증가"
--- oz-backend-django/users/models.py
+++ oz-backend-django/users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model): # Model을 상속받는다
-	name = models.CharField(max_length=20) # 짧은 문장
-	description = models.TextField() # 긴 텍스트 문장
-	age = models.PositiveIntegerField(null=True) # 양의 정수형 숫자 
-	gender = models.CharField(max_length=10)
+class User(AbstractUser):
+    is_business = models.BooleanField(default=False)
+    grade = models.CharField(max_length=10, default='C')
+    
